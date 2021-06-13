fbab4c05cbe4e2d2337a0cc8674cec69930aca40
popsoft01 <popoolatunde52@gmail.com>
2021-06-13 04:26:16 +0100

    new push

--- com/Automated_Emaill.py
+++ com/Automated_Emaill.py
@@ -1,7 +1,28 @@
 import smtplib
+import speech_recognition as sr
+# import PyAudio
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login('popoolatunde52@gmail.com', '08138579886')
-server.sendmail('popoolatunde52@gmail.com', 'poptuma52@yahoo.com',
-                "testing my email bot code and how to automated the sending")
+listner = sr.Recognizer()
+
+
+def get_info():
+    # try:
+        with sr.Microphone() as source:
+            print("listening.......")
+            voice = listner.listen(source)
+            info = listner.recognize_google(voice)
+            print(info)
+            return info.lower()
+
+
+
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('popoolatunde52@gmail.com', '08138579886')
+    server.sendmail('popoolatunde52@gmail.com', 'poptuma52@yahoo.com',
+                    "testing my email bot code and how to automated the sending")
+
+
+get_info()
