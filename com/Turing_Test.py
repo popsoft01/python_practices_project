d28aa5f20da7b818f06f92a1f8ebb3fb6ce0454a
popsoft01 <popoolatunde52@gmail.com>
2021-07-09 01:40:53 +0100

    new push

---
+++ com/Turing_Test.py
@@ -0,0 +1,8 @@
+print("What is your problem?")
+input("Enter your answer")
+print("Have experience before")
+ans = input("Enter yes or no")
+if ans == "yes" or ans == "y":
+    print("Well, you have it again")
+else:
+    print("'Well , you have it now.'")
